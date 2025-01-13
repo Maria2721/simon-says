678280fd1a7f84ce1d2a305be8175f410159c9c0
Maria2721 <mashaivvas@gmail.com>
2025-01-14 02:12:37 +0300

    fix: update base in vite config

--- simon-says/vite.config.js
+++ simon-says/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-	base: "/",
+	base: "/maria2721-JSFE2024Q4/simon-says/",
 	build: {
 		minify: false,
 		terserOptions: {
