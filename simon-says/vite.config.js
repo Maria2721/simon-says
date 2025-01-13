b7982e946278f538b468e79a16922b8df03e149f
Maria2721 <mashaivvas@gmail.com>
2025-01-14 02:02:24 +0300

    fix: update vite config

--- simon-says/vite.config.js
+++ simon-says/vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-	base: "/maria2721-JSFE2024Q4/simon-says/",
+	base: "/",
 	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-			},
-		},
 		minify: false,
-		sourcemap: true,
-	},
-	server: {
-		open: true,
-		port: 3000,
+		terserOptions: {
+			compress: false,
+			mangle: false,
+		},
 	},
 });
