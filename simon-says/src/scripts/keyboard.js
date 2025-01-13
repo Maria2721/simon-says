4cef8d1b4df22485c8f5ff5d2c16f099dc6283cc
Maria2721 <mashaivvas@gmail.com>
2025-01-13 04:06:40 +0300

    feat: add basic html layout with styles for initial screen, function for generate keyboard

---
+++ simon-says/src/scripts/keyboard.js
@@ -0,0 +1,27 @@
+export function generateKeyboard() {
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	const keyboard = document.getElementById("keyboard");
+	const level = document.getElementById("level-select").value;
+
+	keyboard.innerHTML = "";
+
+	let symbols = [];
+	if (level === "easy") {
+		symbols = numbers;
+	} else if (level === "medium") {
+		symbols = letters.split("");
+	} else if (level === "hard") {
+		symbols = numbers.concat(letters.split(""));
+	}
+
+	symbols.forEach((el) => {
+		const key = document.createElement("button");
+		key.id = String(el);
+		key.type = "button";
+		key.innerHTML = String(el);
+		key.className = "keyboard__key";
+		keyboard.appendChild(key);
+	});
+}
--- simon-says/src/scripts/main.js
+++ simon-says/src/scripts/main.js
@@ -1,23 +1,74 @@
 import "normalize.css";
 import "../styles/style.css";
-import { setupCounter } from "./counter.js";
+
+import { generateKeyboard } from "./keyboard.js";
 
 const initializeApp = () => {
 	const app = document.createElement("div");
 	app.className = "container";
 
 	const heading = document.createElement("h1");
-	heading.textContent = "Simon Says!";
+	heading.textContent = "Simon Says game!";
+	heading.className = "heading";
 	app.appendChild(heading);
 
-	const counterButton = document.createElement("button");
-	counterButton.id = "counter";
-	counterButton.type = "button";
-	app.appendChild(counterButton);
+	const options = document.createElement("div");
+	options.className = "options";
+
+	const startButton = document.createElement("button");
+	startButton.id = "start-button";
+	startButton.type = "button";
+	startButton.innerHTML = "Start";
+	startButton.className = "start__btn";
+	options.appendChild(startButton);
+
+	const selectWrapper = document.createElement("div");
+	selectWrapper.className = "select__wrapper";
+
+	const levelLabel = document.createElement("label");
+	levelLabel.htmlFor = "level-select";
+	levelLabel.className = "level__label";
+	levelLabel.innerHTML = "Choose level";
+	selectWrapper.appendChild(levelLabel);
+
+	const levelSelect = document.createElement("select");
+	levelSelect.id = "level-select";
+	levelSelect.name = "levels";
+	levelSelect.className = "level__select";
+
+	const option1 = document.createElement("option");
+	option1.value = "easy";
+	option1.innerHTML = "Easy";
+	option1.className = "level__option";
+	option1.selected = true;
+	levelSelect.appendChild(option1);
+
+	const option2 = document.createElement("option");
+	option2.value = "medium";
+	option2.innerHTML = "Medium";
+	option2.className = "level__option";
+	levelSelect.appendChild(option2);
+
+	const option3 = document.createElement("option");
+	option3.value = "hard";
+	option3.innerHTML = "Hard";
+	option3.className = "level__option";
+	levelSelect.appendChild(option3);
+
+	selectWrapper.appendChild(levelSelect);
+	options.appendChild(selectWrapper);
+	app.appendChild(options);
+
+	const keyboard = document.createElement("div");
+	keyboard.id = "keyboard";
+	keyboard.className = "keyboard__wrapper";
+	app.appendChild(keyboard);
 
 	document.body.appendChild(app);
-
-	setupCounter(counterButton);
 };
 
 initializeApp();
+generateKeyboard();
+
+const levelsSelect = document.getElementById("level-select");
+levelsSelect.addEventListener("click", generateKeyboard);
